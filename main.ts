29579b597d079566357be9bd8eedfd023a17da9b
GrandPa <75545155+grandparon@users.noreply.github.com>
2020-12-11 16:25:50 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+let our_random_number = 0
+basic.forever(function () {
+    our_random_number = randint(1, 6)
+    if (1 == our_random_number) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (2 == our_random_number) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(500)
+    } else if (3 == our_random_number) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (4 == our_random_number) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (5 == our_random_number) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+    }
+})
